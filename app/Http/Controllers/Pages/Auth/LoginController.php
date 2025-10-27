8f10a994ad30cb9dc5b0b9525df140c572dc4f4c
gibihu <nawapol0010@gmail.com>
2025-10-28 01:03:23 +0700

    change route

--- app/Http/Controllers/Pages/Auth/LoginController.php
+++ app/Http/Controllers/Pages/Auth/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
 
             if (Auth::attempt($credentials, $request->boolean('remember'))) {
                 $request->session()->regenerate();
-                return redirect()->intended(route('dash.index', absolute: true))->with('reload', true);
+                return redirect()->intended(route('home', absolute: true))->with('reload', true);
             }
 
             throw ValidationException::withMessages([
